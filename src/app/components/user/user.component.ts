90c205b3f286ad61dc590f9b91f108a1051931a7
sagun <sagun@example.com>
2018-06-03 19:40:05 -0400

    angular 4

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
+})
+export class UserComponent implements OnInit {
+  name: string;
+  age: number;
+  email: string;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+  };
+  hobbies: string[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.name = "John";
+    this.age = 30;
+    this.address = {
+      street: "402 filhiol",
+      city: "Bsoton",
+      state: "MA"
+    };
+
+    this.hobbies = ["write code", "watch movie"];
+  }
+
+  onClick() {
+    this.name = "Sagun";
+    this.hobbies.push("New Hobby");
+  }
+
+  addHobby(hobby) {
+    this.hobbies.push(hobby);
+    return false;
+  }
+}
